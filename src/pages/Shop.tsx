011a1e13500ef5e5c0402f53d60e6c88f9a08f26
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-01-16 13:55:22 +0700

    update

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -53,7 +53,7 @@ const Shop = () => {
           </div>
 
           {/* Products Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6 md:gap-8">
             {filteredProducts.map((product, index) => (
               <div
                 key={product.id}
